75addc3baa28466ead6b9ef41655f418b1f3c46d
aditikashyap914 <aditikashyap914@gmail.com>
2024-07-10 11:51:01 +0530

    Server code is initiated

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+// console.log("Hello world");
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server running at PORT ${PORT}`));
