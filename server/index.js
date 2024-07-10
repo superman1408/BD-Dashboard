aca3d06b89e14cae91ab78ead2f9ac00c057a673
superman1408 <superman14082017@gmail.com>
2024-07-10 12:29:35 +0530

    Configuring server side

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-// console.log("Hello world");
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server running at PORT ${PORT}`));
+
+app.get('/', (req, res) => {
+    console.log('Hello World!');
+    res.send(`Server is running at PORT:  ${PORT}`);
+});
+
+
+app.listen(PORT, () => console.log(`Server running at PORT:  ${PORT}`));
