7f819958e1be6022796fa1eba0fa3a35e5af2ff9
aditikashyap914 <aditikashyap914@gmail.com>
2024-07-10 12:04:35 +0530

    Route added...

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 // console.log("Hello world");
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
+app.use(cors());
+
 const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("HEllo ASHKAM API");
+});
 
 app.listen(PORT, () => console.log(`Server running at PORT ${PORT}`));
