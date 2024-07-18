eb58f4baa1f5efd05a44d4ea27e7f00ad0a8cad7
aditikashyap914 <aditikashyap914@gmail.com>
2024-07-18 17:21:54 +0530

    Navbar updated

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import { Container, Typography } from "@mui/material";
 
 const Dashboard = () => {
   return (
     <div>
       <Navbar />
-      Dashboard
+      <Container
+        sx={{ display: "flex", justifyContent: "center", height: "400px" }}
+      >
+        <Typography variant="h5">Welcome to ASHKAM</Typography>
+      </Container>
     </div>
   );
 };
