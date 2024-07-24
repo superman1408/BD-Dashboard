b8a67743c10354d6debcf05eb1995789cb287da0
aditikashyap914 <aditikashyap914@gmail.com>
2024-07-24 16:19:39 +0530

    datasheet form is created

--- client/src/components/DataSheet/DataSheet.js
+++ client/src/components/DataSheet/DataSheet.js
@@ -16,48 +16,49 @@ const DataSheet = () => {
         }}
       >
         <form onSubmit={handleSubmit} className="time-sheet-form">
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <div className="form-group">
-              <label
-                style={{ color: "#16355d", fontFamily: "Roboto" }}
-                htmlFor="date"
-              >
-                S.No.
-              </label>
-              <input
-                type="date"
-                id="date"
-                //   defaultValue={date}
-                //   // onChange={(e) => setDate(e.target.value)}
-                //   onChange={handleCheck}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label
-                style={{ color: "#16355d", fontFamily: "Roboto" }}
-                htmlFor="projectCode"
-              >
-                Bid No.
-              </label>
+          <Grid sx={{ display: "flex", flexDirection: "column" }}>
+            <Grid sx={{ display: "flex", flexdirection: "column" }}>
+              <Grid>
+                <label
+                  style={{ color: "#16355d", fontFamily: "Roboto" }}
+                  htmlFor="date"
+                >
+                  S.No.
+                </label>
+              </Grid>
+              <Grid>
+                <input type="date" id="date" required />
+              </Grid>
+            </Grid>
 
-              <input
-                style={{
-                  // width: "100%",
-                  height: "30px",
-                  padding: "8px",
-                  fontSize: "16px",
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                  color: "#e55d17",
-                }}
-                type="text"
-                id="projectCode"
-                // value={projectCode}
-                //   defaultValue={projectCode}
-                //   onFocus={togglePopup1} // Using onFocus event to trigger the popup
-                autoComplete="off"
-              />
+            <Grid
+              className="form-group"
+              sx={{ display: "flex", flexdirection: "column" }}
+            >
+              <Grid>
+                <label
+                  style={{ color: "#16355d", fontFamily: "Roboto" }}
+                  htmlFor="projectCode"
+                >
+                  Bid No.
+                </label>
+              </Grid>
+              <Grid>
+                <input
+                  style={{
+                    // width: "100%",
+                    height: "30px",
+                    padding: "8px",
+                    fontSize: "16px",
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                    color: "#e55d17",
+                  }}
+                  type="text"
+                  id="projectCode"
+                  autoComplete="off"
+                />
+              </Grid>
               {/* ______________________________________pop window contents_____________________________________________ */}
 
               {/* {projectopen && (
@@ -66,103 +67,110 @@ const DataSheet = () => {
                 setProjectOpen={setProjectOpen}
               />
             )} */}
-            </div>
+            </Grid>
 
-            <div className="form-group">
-              <label
-                style={{ color: "#16355d", fontFamily: "Roboto" }}
-                htmlFor="activityCode"
-              >
-                Client Name
-              </label>
-              <input
-                style={{
-                  // width: "100%",
-                  height: "30px",
-                  padding: "8px",
-                  fontSize: "16px",
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                  color: "#e55d17",
-                }}
-                type="text"
-                id="activityCode"
-                // value={activityCode}
-                //   defaultValue={activityCode}
-                //   onFocus={togglePopup2}
-                autoComplete="off"
-              />
+            <Grid
+              className="form-group"
+              sx={{ display: "flex", flexdirection: "column" }}
+            >
+              <Grid>
+                <label
+                  style={{ color: "#16355d", fontFamily: "Roboto" }}
+                  htmlFor="activityCode"
+                >
+                  Client Name
+                </label>
+              </Grid>
+              <Grid>
+                <input
+                  style={{
+                    // width: "100%",
+                    height: "30px",
+                    padding: "8px",
+                    fontSize: "16px",
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                    color: "#e55d17",
+                  }}
+                  type="text"
+                  id="activityCode"
+                  autoComplete="off"
+                />
+              </Grid>
               {/* {activityopen && (
               <ActivityCodePopUp
                 setActivityCode={setActivityCode}
                 setActivityOpen={setActivityOpen}
               />
             )} */}
-            </div>
+            </Grid>
 
-            <div className="form-group">
-              <label
-                style={{ color: "#16355d", fontFamily: "Roboto" }}
-                htmlFor="netTime"
-              >
-                Opportunity Detail
-              </label>
-              <input
-                type="number"
-                id="netTime"
-                // value={netTime}
-                //   defaultValue={netTime}
-                //   onChange={(e) => setNetTime(e.target.value)}
-                // max={8}
-              />
-            </div>
-            <div className="form-group">
-              <label
-                style={{ color: "#16355d", fontFamily: "Roboto" }}
-                htmlFor="overTime"
-              >
-                Probability
-              </label>
-              <input
-                type="number"
-                id="overTime"
-                // value={overTime}
-                //   defaultValue={overTime}
-                //   onChange={(e) => setOverTime(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label
-                style={{ color: "#16355d", fontFamily: "Roboto" }}
-                htmlFor="overTime"
-              >
-                Amount
-              </label>
-              <input
-                type="number"
-                id="overTime"
-                // value={overTime}
-                //   defaultValue={overTime}
-                //   onChange={(e) => setOverTime(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label
-                style={{ color: "#16355d", fontFamily: "Roboto" }}
-                htmlFor="overTime"
-              >
-                Weitage Amount
-              </label>
-              <input
-                type="number"
-                id="overTime"
-                // value={overTime}
-                //   defaultValue={overTime}
-                //   onChange={(e) => setOverTime(e.target.value)}
-              />
-            </div>
+            <Grid
+              className="form-group"
+              sx={{ display: "flex", flexdirection: "column" }}
+            >
+              <Grid>
+                <label
+                  style={{ color: "#16355d", fontFamily: "Roboto" }}
+                  htmlFor="netTime"
+                >
+                  Opportunity Detail
+                </label>
+              </Grid>
+              <Grid>
+                <input type="number" id="netTime" />
+              </Grid>
+            </Grid>
+            <Grid
+              className="form-group"
+              sx={{ display: "flex", flexdirection: "column" }}
+            >
+              <Grid>
+                <label
+                  style={{ color: "#16355d", fontFamily: "Roboto" }}
+                  htmlFor="overTime"
+                >
+                  Probability
+                </label>
+              </Grid>
+              <Grid>
+                <input type="number" />
+              </Grid>
+            </Grid>
+            <Grid
+              className="form-group"
+              sx={{ display: "flex", flexdirection: "column" }}
+            >
+              <Grid>
+                <label
+                  style={{ color: "#16355d", fontFamily: "Roboto" }}
+                  htmlFor="overTime"
+                >
+                  Amount
+                </label>
+              </Grid>
+              <Grid>
+                <input type="number" />
+              </Grid>
+            </Grid>
+            <Grid
+              className="form-group"
+              sx={{ display: "flex", flexdirection: "column" }}
+            >
+              <Grid>
+                <label
+                  style={{ color: "#16355d", fontFamily: "Roboto" }}
+                  htmlFor="overTime"
+                >
+                  Weitage Amount
+                </label>
+              </Grid>
+              <Grid>
+                <input type="number" id="overTime" />
+              </Grid>
+            </Grid>
             {/* </div> */}
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <Grid style={{ display: "flex", justifyContent: "space-around" }}>
               <button style={{ fontFamily: "Roboto" }} type="submit">
                 {" "}
                 Submit
@@ -170,8 +178,8 @@ const DataSheet = () => {
               <button style={{ fontFamily: "Roboto" }} type="button">
                 Clear
               </button>
-            </div>
-          </div>
+            </Grid>
+          </Grid>
         </form>
       </Grid>
     </div>
