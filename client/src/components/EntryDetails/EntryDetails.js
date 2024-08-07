dbba290682785b2ebc42f9a885eb86b852a752b8
aditikashyap914 <aditikashyap914@gmail.com>
2024-08-07 18:07:55 +0530

    entry detail form created

--- client/src/components/EntryDetails/EntryDetails.js
+++ client/src/components/EntryDetails/EntryDetails.js
@@ -1,18 +1,41 @@
 import React from "react";
-import { Card, Container } from "@mui/material";
+import { Card, Container, Grid } from "@mui/material";
 
 const EntryDetails = () => {
   return (
-    <Container>
-      <Card sx={{ justifyContent: "center", marginLeft: "50px" }}>
-        <typography variant="h4">Entry Detail Page</typography>
+    <Grid
+      padding="10px"
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Card
+        elevation={10}
+        sx={{
+          justifyContent: "center",
+          padding: "20px",
+          backgroundColor: "#f2f2f2",
+          borderCollapse: "collapse",
+          marginBottom: "130px",
+          // border: "1px solid black",
+        }}
+      >
+        <h3
+          style={{
+            textAlign: "center",
+            fontFamily: "Roboto ",
+            color: "#0d325c",
+          }}
+        >
+          Entry Detail Form
+        </h3>
         <label>project Name</label>
         <input type="text" placeholder="Enter Project Name" />
         <label>Date</label>
         <input type="date" placeholder="Enter Project Name" />
-        <label>Month</label>
-        <input type="month" placeholder="Enter Project Name" />
-        <label>Activities</label>
+        <label>Month</label>0 <label>Activities</label>
         <input type="activities" placeholder="Enter Project Name" />
         <div>
           <label>Civil & Structure</label>
@@ -29,7 +52,7 @@ const EntryDetails = () => {
         <label>Female labour</label>
         <textarea />
       </Card>
-    </Container>
+    </Grid>
   );
 };
 
