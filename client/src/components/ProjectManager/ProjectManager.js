70e538a958e5d908e412a985071e9d95bc131bd0
aditikashyap914 <aditikashyap914@gmail.com>
2025-03-28 17:54:25 +0530

    ProjectManager module updated

--- client/src/components/ProjectManager/ProjectManager.js
+++ client/src/components/ProjectManager/ProjectManager.js
@@ -1,114 +1,189 @@
 import React from "react";
-import { Grid, IconButton } from "@mui/material";
+import {
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
 
 import ListIcon from "@mui/icons-material/List";
 import NoteIcon from "@mui/icons-material/Note";
 import Diversity2Icon from "@mui/icons-material/Diversity2";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import MailIcon from "@mui/icons-material/Mail";
+import LogoutIcon from "@mui/icons-material/Logout";
+import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
+import TopicIcon from "@mui/icons-material/Topic";
+
 const ProjectManager = () => {
+  const menuItems = [
+    { text: "Project Window", icon: <DashboardIcon />, link: "/projectwindow" },
+    {
+      text: "Contract Register",
+      icon: <AppRegistrationIcon />,
+      link: "/contractregister",
+    },
+    {
+      text: "Contract Details",
+      icon: <TopicIcon />,
+      link: "/contractviewdetails",
+    },
+    {
+      text: "Letter Tracker",
+      icon: <MailIcon />,
+      link: "/lettertracker",
+    },
+    {
+      text: "Time Sheet Display",
+      icon: <MailIcon />,
+      link: "/timesheet",
+    },
+    {
+      text: "Tasks",
+      icon: <MailIcon />,
+      link: "/tasks",
+    },
+    {
+      text: "Task Completed",
+      icon: <MailIcon />,
+      link: "/completed/:status",
+    },
+    {
+      text: "Task in-progress",
+      icon: <MailIcon />,
+      link: "/in-progress/:status",
+    },
+    {
+      text: "Todo",
+      icon: <MailIcon />,
+      link: "/todo/:status",
+    },
+  ];
+
   return (
     <div style={{ marginBottom: "10px" }}>
       <Grid sx={{ display: "flex", flexDirection: "row" }}>
-        <Grid>
-          <h1 className="text-lg font-normal text-500 ml-[20px] mr-[80px]">
-            Project Manager
-          </h1>
-        </Grid>
-        <Grid className="flex flex-row items-center gap-2">
-          <IconButton
-            size="small"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <CalendarMonthIcon />
-          </IconButton>
-          <h1 className="text-sm font-normal text-black-500  mr-[40px]">
-            MY SUMMARY
-          </h1>
-        </Grid>
-        <Grid className="flex flex-row items-center gap-2">
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            // onClick={}
-            color="inherit"
-          >
-            <Diversity2Icon />
-          </IconButton>
+        <Grid sx={{ display: "flex", flexDirection: "column" }}>
+          <Grid>
+            <h1 className="text-lg font-normal text-500 ml-[20px] mr-[80px]">
+              Project Manager
+            </h1>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText />
+                </ListItemButton>
 
-          <h1 className="text-sm font-normal text-black-500  mr-[40px]">
-            TEAM SUMMARY
-          </h1>
+                <ListItemButton>
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Grid>
         </Grid>
-        <Grid className="flex flex-row items-center gap-2">
-          <IconButton
-            size="small"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <CalendarMonthIcon />
-          </IconButton>
-          <h1 className="text-sm font-normal text-black-500  mr-[40px]">
-            Portfolio SUMMARY
-          </h1>
-        </Grid>
-        <Grid className="flex flex-row items-center gap-2">
-          <ListIcon sx={{ margin: "2px" }} />
-          <h1 className="text-sm font-normal text-black-500 ml-[20px] mr-[40px]">
-            LIST
-          </h1>
-        </Grid>
-        <Grid className="flex flex-row items-center gap-2">
-          <IconButton
-            size="small"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <CalendarMonthIcon />
-          </IconButton>
-          <h1 className="text-sm font-normal text-black-500  mr-[40px]">
-            BOARD
-          </h1>
-        </Grid>
+        <Grid sx={{ display: "flex", flexDirection: "row" }}>
+          <Grid className="flex flex-row items-center gap-2">
+            <IconButton
+              size="small"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <CalendarMonthIcon />
+            </IconButton>
+            <h1 className="text-sm font-normal text-black-500  mr-[40px]">
+              MY SUMMARY
+            </h1>
+          </Grid>
+          <Grid className="flex flex-row items-center gap-2">
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              // onClick={}
+              color="inherit"
+            >
+              <Diversity2Icon />
+            </IconButton>
 
-        <div className="flex flex-row items-center gap-2">
-          <IconButton
-            size="small"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <CalendarMonthIcon />
-          </IconButton>
-          <h1 className="text-sm font-normal text-black-500 mr-[40px]">
-            CALENDAR
-          </h1>
-        </div>
+            <h1 className="text-sm font-normal text-black-500  mr-[40px]">
+              TEAM SUMMARY
+            </h1>
+          </Grid>
+          <Grid className="flex flex-row items-center gap-2">
+            <IconButton
+              size="small"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <CalendarMonthIcon />
+            </IconButton>
+            <h1 className="text-sm font-normal text-black-500  mr-[40px]">
+              Portfolio SUMMARY
+            </h1>
+          </Grid>
+          <Grid className="flex flex-row items-center gap-2">
+            <ListIcon sx={{ margin: "2px" }} />
+            <h1 className="text-sm font-normal text-black-500 ml-[20px] mr-[40px]">
+              LIST
+            </h1>
+          </Grid>
+          <Grid className="flex flex-row items-center gap-2">
+            <IconButton
+              size="small"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <CalendarMonthIcon />
+            </IconButton>
+            <h1 className="text-sm font-normal text-black-500  mr-[40px]">
+              BOARD
+            </h1>
+          </Grid>
 
-        <Grid className="flex flex-row items-center gap-2">
-          <IconButton
-            size="small"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            // onClick={}
-            color="inherit"
-          >
-            <NoteIcon />
-          </IconButton>
-          <h1 className="text-sm font-normal text-black-500  mr-[40px]">
-            FILES
-          </h1>
+          <div className="flex flex-row items-center gap-2">
+            <IconButton
+              size="small"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <CalendarMonthIcon />
+            </IconButton>
+            <h1 className="text-sm font-normal text-black-500 mr-[40px]">
+              CALENDAR
+            </h1>
+          </div>
+
+          <Grid className="flex flex-row items-center gap-2">
+            <IconButton
+              size="small"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              // onClick={}
+              color="inherit"
+            >
+              <NoteIcon />
+            </IconButton>
+            <h1 className="text-sm font-normal text-black-500  mr-[40px]">
+              FILES
+            </h1>
+          </Grid>
         </Grid>
       </Grid>
       {/* <div className="overflow-auto   mt-3">
@@ -133,9 +208,7 @@ const ProjectManager = () => {
           </table>
         </div>
       </div> */}
-      <Grid>
-        
-      </Grid>
+      <Grid></Grid>
     </div>
   );
 };
