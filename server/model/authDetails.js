10e54f1b41ad1fe761f34549a04cd8928813dd59
aditikashyap914 <aditikashyap914@gmail.com>
2025-04-14 10:45:00 +0530

    model for role and menhour is created

--- server/model/authDetails.js
+++ server/model/authDetails.js
@@ -1,24 +1,34 @@
 import mongoose from "mongoose";
-const authSchema = mongoose.Schema({
-  firstName: {
-    type: String,
+const authSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    confirmPassword: {
+      type: String,
+    },
+    menHourCost: {
+      type: String,
+    },
+    menHourLeft: {
+      type: String,
+    },
+    role: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  confirmPassword: {
-    type: String,
-  },
-},
-{
-  timestamps: true,
-}
+  {
+    timestamps: true,
+  }
 );
 
 const AuthDetails = mongoose.model("AuthDetails", authSchema);
