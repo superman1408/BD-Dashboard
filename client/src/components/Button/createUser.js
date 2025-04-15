ecee69474c87fa5d248fa3d69ff4c675ca60118e
aditikashyap914 <aditikashyap914@gmail.com>
2025-04-15 10:45:13 +0530

    Button for createUser is ceated

---
+++ client/src/components/Button/createUser.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+import Fab from "@mui/material/Fab";
+
+const createUser = () => {
+  return (
+    <div>
+      {" "}
+      <Grid sx={{ margin: "20px" }}>
+        <Fab
+          variant="extended"
+          size="small"
+          color="primary"
+          sx={{ mr: 1 }}
+          onClick={openDialog}
+        >
+          <AddIcon sx={{ mr: 1 }} />
+          Create new user
+        </Fab>
+      </Grid>
+    </div>
+  );
+};
+
+export default createUser;
