56ca7c93c3995742d9283d9098e55d8eab8850b4
Farheen Kaifee <farheenkaifee555@gmail.com>
2025-07-14 11:38:32 +0530

    Dont know just created branch

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import contractRoutes from "./routes/contract.js";
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const CONNECT = process.env.CONNECTION_URL;
 
